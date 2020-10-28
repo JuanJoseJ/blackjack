94f0dc38f9a34b58a0a4b19bfad13d61cfb9ca41
JuanJoseJ <jjj.1998@hotmail.com>
2020-10-28 07:09:23 -0500

    Cosa

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/templates/index.html');
 });
 
+app.get('/blackjack', (req, res) => {
+    res.sendFile(__dirname + '/templates/game.html');
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
